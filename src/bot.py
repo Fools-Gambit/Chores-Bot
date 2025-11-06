1d0f06b0735161304c1043801ba5a7d049e35be3
Andrew McCollister <andrewmccollister1995@gmail.com>
2025-11-05 21:49:49 -0600

    Cleaning up pseudocode

--- src/bot.py
+++ src/bot.py
@@ -18,6 +18,7 @@ import util
 # =================================
 # Logging setup
 # =================================
+# TODO: Adjust the logging functionality
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
@@ -92,10 +93,6 @@ Now Serving.\n""".format('\n'.join(u.nick or u.name for u in users),
   print(server_str, flush=True)
   guild = discord.Object(id=int(os.getenv('GUILD')))
   await bot.tree.sync(guild=guild)
-  #synced = await bot.tree.fetch_commands(guild=guild)
-  #print(f"Ready! Synced {len(synced)} commands to YOUR server")
-  #print("Go type / in any channel → create_bundle is there!")
-
 
 #Bundles
 #   Kitchen (a) - countertops, stove, sink, airfryer, microwave, fridge, oven
@@ -128,22 +125,20 @@ Now Serving.\n""".format('\n'.join(u.nick or u.name for u in users),
 
 @bot.hybrid_command(name='create_bundle', description='Adds specified bundle and adds it to the rotation')
 @app_commands.describe(bundle='the chore bundle')
-async def create_bundle(ctx, bundle: str):
-  #TODO: Possibly send user empty prompt and add_chore()
+async def create_bundle(ctx: commands.Context, bundle: str):
   parsed = bundle.split()
   sch.add_chore(parsed)
   # Run the set_rotation function
-  #await ctx.message.channel.send(f'Bundle {parsed[0]} has been added to rotation and bundles')
   await ctx.send(f'Bundle {parsed[0]} has been added to rotation and bundles')
 
 '''@bot.hybrid_command(name='delete_bundle', description='Delete specified bundle and removes it from rotation')
 @app_commands.describe()
-async def FUNCTION_NAME(ctx):
+async def delete_bundle(ctx):
   send user ('what bundle would you like to delete')
   send user dropdown of "bundles"
     delete chosen bundle
   run set rotation function
-  await ctx.message.channel.send('Bundle "x" has been deleted and removed from rotation')'''
+  await ctx.send('Bundle "x" has been deleted and removed from rotation')'''
 
 ''' Not an important command right now
 @bot.command(name='update_bundle', help='Enter new responsibilities for selected bundle')
@@ -153,7 +148,7 @@ async def update_bundle(ctx):
   send user empty prompt
     export text as string"new_responsibilities"
     replace string"responsibilities" of "chosen_bundle" with string"new_responsibilities"
-  await ctx.message.channel.send('Bundle "z" is now responsible for "responsibilities"')
+  await ctx.send('Bundle "z" is now responsible for "responsibilities"')
   return
 '''  
 
@@ -162,28 +157,28 @@ async def update_bundle(ctx):
 async def show_bundle(ctx):
   collect all bundle and
 #   format into varible named "output"
-  await ctx.message.channel.send(print "output")
+  await ctx.send(print "output")
   return
 
 
 
 @bot.command(name='show_rotation', help='Displays the current rotation of Who does What')
-async def FUNCTION_NAME(ctx):
+async def show_rotation(ctx):
   format "rotations" 
-  await ctx.message.channel.send(print "rotations")
+  await ctx.send(print "rotations")
   return
 
 @bot.command(name='set_rotation', help='Select what weeks rotation you'd like it set to')
-async def rotation_weeks(ctx):
+async def set_rotation(ctx):
   send user dropdown of "weeks"
   run set rotation function
-  await ctx.message.channel.send(MESSAGE_TEXT)
+  await ctx.send(MESSAGE_TEXT)
   return
 
 @bot.command(name='complete', help='Marks the users assigned tasks as completed')
 async def complete(ctx):
   set user task as complete
-  await ctx.message.channel.send(you did good nigga)
+  await ctx.send(you did good nigga)
   return
 '''
 
@@ -218,7 +213,6 @@ async def complete(ctx):
 #     delta.total_seconds()))
 #   await asyncio.sleep(delta.total_seconds())
 #   return
-
   
 if __name__ == '__main__':
   util.load_env()
